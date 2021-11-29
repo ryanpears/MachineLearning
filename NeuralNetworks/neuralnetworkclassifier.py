80925a03bc481b80b4848fef15470eb80c81349b
ryanpears <pryan78@yahoo.com>
2021-11-28 19:24:57 -0700

    basic NN setup

---
+++ NeuralNetworks/neuralnetworkclassifier.py
@@ -0,0 +1,40 @@
+import pandas
+import sys
+import numpy as np
+
+
+LABEL = ''
+FEATURES = []
+
+# sigmoid
+def sigmoid(x):
+  return 1.0/(1.0+np.exp(-x))
+
+
+# file reading
+def read_columns(file_path):
+  with open(file_path, 'r') as file:
+    columns = []
+    for line in file:
+      columnsInLine = line.strip().split(",")
+      columns += columnsInLine
+    return columns
+
+if __name__ == "__main__":
+  train_csv = sys.argv[1]
+  test_csv = sys.argv[2]
+  columns_txt = sys.argv[3]
+
+  columns = read_columns(columns_txt)
+  LABEL = columns[-1]
+  FEATURES = columns[:-1]
+
+  train_df = pandas.read_csv(train_csv, names=columns, index_col=False)
+  test_df = pandas.read_csv(test_csv, names=columns, index_col=False)
+
+  # format data
+  # not sure if I need this
+  # train_df[LABEL] = train_df[LABEL].apply(lambda x: 1 if x == 1 else -1)
+  # test_df[LABEL] = test_df[LABEL].apply(lambda x: 1 if x == 1 else -1)
+  train_df.insert(loc=0, column="baisvalue", value=1)
+  test_df.insert(loc=0, column="baisvalue", value=1)
